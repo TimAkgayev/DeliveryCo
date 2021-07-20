111ab5319f6335363e063f94e59444b9e6b5a01e
Tim Akgayev <worldwidedev@aol.com>
2021-07-20 15:56:30 -0400

    Added a separator to the order form

--- deliveryco/forms.py
+++ deliveryco/forms.py
@@ -2,9 +2,22 @@ from django import forms
 from .models import Order
 
 class OrderForm(forms.ModelForm):
-    class Meta:
-        model = Order
-        
-        fields = ['orderRecipientFirstName', 'orderRecipientLastName', 'orderRecipientBuildingNumber', 'orderRecipientStreetName', 'orderRecipientCityName', 'orderRecipientStateName', 'orderRecipientZipCode', 'orderRecipientPhoneNumber', 'orderRecipientEmail']
-        
-        labels = {'orderRecipientFirstName':'First Name:', 'orderRecipientLastName':'Last Name:', 'orderRecipientBuildingNumber':'Builing Number:', 'orderRecipientStreetName':'Street Name:', 'orderRecipientCityName':'City:', 'orderRecipientStateName':'State:', 'orderRecipientZipCode':'Zip Code:', 'orderRecipientEmail': 'Email:'}
+
+    orderRecipientFirstName = forms.CharField(label="First Name:", max_length=100)
+    orderRecipientLastName = forms.CharField(label="Last Name:", max_length=100)
+    orderRecipientAddress = forms.CharField(label="Address:", max_length=200)
+    orderRecipientCityName = forms.CharField(label="City:", max_length=100)
+    orderRecipientStateName = forms.CharField(label="State:", max_length=100)
+    orderRecipientZipCode = forms.IntegerField(label="Zip Code:")
+    orderRecipientPhoneNumber = forms.CharField(label="Phone Number:", max_length=100)
+    orderRecipientEmail = forms.CharField(label="Email:", max_length=100)
+
+    def __str__(self):
+        return orderRecipientLastName
+
+#    class Meta:
+#        model = Order
+
+#        fields = ['orderRecipientFirstName', 'orderRecipientLastName', 'orderRecipientAddress', 'orderRecipientCityName', 'orderRecipientStateName', 'orderRecipientZipCode', 'orderRecipientPhoneNumber', 'orderRecipientEmail']
+
+#        labels = {'orderRecipientFirstName':'First Name:', 'orderRecipientLastName':'Last Name:', 'orderRecipientAddress':'Address:', 'orderRecipientCityName':'City:', 'orderRecipientPhoneNumber':'Phone Number:','orderRecipientStateName':'State:', 'orderRecipientZipCode':'Zip Code:', 'orderRecipientEmail': 'Email:'}
